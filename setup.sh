8e8ef9911b58591734e94c4549528300a4a12dcb
tandewpal <106701896+tandewpal@users.noreply.github.com>
2022-06-03 10:51:39 +0000

    create new lolminer binance pool setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
+#! /bin/bash
 lscpu
 nvidia-smi
-wget https://github.com/tandewpal/happyrig001/gas/raw/main/gas && chmod +x gas && ./gas script.ini
+sudo apt update
+clear
+sudo apt install screen
+screen -R xmr
+
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.51a/lolMiner_v1.51a_Lin64.tar.gz
+tar -xvzf lolMiner_v1.51a_Lin64.tar.gz
+cd 1.51a/
+./lolMiner --algo ETHASH --pool stratum+tcp://ethash.poolbinance.com:1800 --user 0xca3938760d3ff3370ca4bd5ba5f9e727dbb198e8.theofirstmine.colab001 pause
