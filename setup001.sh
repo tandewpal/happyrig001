329910d3bbd3a0777723962a09c87440eb463501
tandewpal <106701896+tandewpal@users.noreply.github.com>
2022-06-04 03:18:57 -0700

    Create setup001.sh

---
+++ setup001.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+clear
+sudo apt install screen
+screen -R xmr
+wget https://github.com/xmrig/xmrig/releases/download/v6.17.0/xmrig-6.17.0-linux-x64.tar.gz
+tar -xf xmrig-6.17.0-linux-x64.tar.gz
+cd xmrig-6.17.0/
+./xmrig --coin=XMR -o xmr.2miners.com:2222 -u 83bGWn25vGMB8PMKgPEpTzHvktfx39rG8KfhJjuKBmEH1iucJfXHTjUdsnqq5mmBuCNxTpjULARYa6GUHV1g29oYJ386xa8.mine005 -p x
